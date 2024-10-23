3927843706f835e45ef5dc3804438bba952b5ee5
JonathanAndresM <jonathanandres.elyonny@gmail.com>
2024-10-22 22:44:24 -0300

    finish?

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'home-main': "url('src/assets/savvas-kalimeris-hO3do8FKJkQ-unsplash.jpg')",
+        'home-main': "url('https://images.unsplash.com/photo-1645036995768-bd4ea2589808?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       },
       keyframes: {
         fadeInOut: {
